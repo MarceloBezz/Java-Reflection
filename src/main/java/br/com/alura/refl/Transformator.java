f027f65ded6978d57c7517eddcdb2f3dd66b3cf9
Marcelo <mdbezerra16@gmail.com>
2025-05-30 11:54:18 -0300

    feat: :sparkles: Obtendo a classe-alvo no método transform() e retornando sua instância

--- src/main/java/br/com/alura/refl/Transformator.java
+++ src/main/java/br/com/alura/refl/Transformator.java
@@ -1,9 +1,19 @@
 package br.com.alura.refl;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
 public class Transformator {
     
-    public <I, O> void transform(I input, Class target) throws ClassNotFoundException {
-        Class<?> sourceClass = input.getClass();
-        Class<?> targetClass = target;
+    public <I, O> O transform(I input) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+        Class<?> source = input.getClass();
+        Class<?> target = Class.forName(source.getName() + "DTO");
+
+       O targetClass = (O) target.getDeclaredConstructor().newInstance();
+
+       Field[] sourcesFields = source.getDeclaredFields();
+       Field[] targetFields = target.getDeclaredFields();
+
+       return targetClass;
     }
 }
